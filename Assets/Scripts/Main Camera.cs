9194a4289066d17da36c93cfbec599c2f66d21f1
track394 <track394@yahoo.com>
2018-09-17 11:28:22 -0700

    Changed class name of Main Camera script

--- Assets/Scripts/Main Camera.cs
+++ Assets/Scripts/Main Camera.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class Main_Camera : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
