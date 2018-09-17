7f43b68b824a4fc6a4ee4a7634de73174497e45b
track394 <track394@yahoo.com>
2018-09-17 12:25:03 -0700

    Added component references to player controller to main camera and game manager.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public GameObject GameManager;
+    public GameObject MainCamera;
+
 	// Use this for initialization
 	void Start () {
-		
+        GameManager = GetComponent<GameObject>();
+        MainCamera = GetComponent<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Debug.Log(Input.GetAxis("Horizontal"));
 	}
 }
