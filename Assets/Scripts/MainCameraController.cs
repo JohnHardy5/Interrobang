2a436317e40c102c612b360f6814e68b4cd3b52c
track394 <track394@yahoo.com>
2018-09-17 12:31:10 -0700

    Started on camera rotation scripts.

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour {
 
+    public GameObject GameManager;
+    private Transform CameraTransform;
+
 	// Use this for initialization
 	void Start () {
-		
+        GameManager = GetComponent<GameObject>();
+        CameraTransform = this.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public bool RotateHorizontal(float turnRate)
+    {
+        return true;
+    }
 }
