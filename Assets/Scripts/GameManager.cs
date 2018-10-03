20f664308278e066596c30ccbc3a6aa345a5deb9
track394 <track394@yahoo.com>
2018-10-02 19:37:49 -0700

    Got player death working. Player dies if they fall out of level.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,10 +12,30 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     //Game settings
-    public float mouseSensitivity;
+    public float mouseSensitivity;//TODO: Make adjustable mouse sensitivity
+    public float pointOfNoReturn;//Point at which player has fallen out of map
+
+    public GameObject playerGO;
+    private UnityStandardAssets.Characters.FirstPerson.FirstPersonController playerScript;
+    private Rigidbody playerRB;
+    private Transform playerT;
 
     // Use this for initialization
     void Start () {
-        //Cursor.lockState = CursorLockMode.Locked;
-	}
+        playerScript = playerGO.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>();
+        playerRB = playerGO.GetComponent<Rigidbody>();
+        playerT = playerGO.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (playerT.position.y < pointOfNoReturn) StartCoroutine(KillPlayer());
+    }
+
+    IEnumerator KillPlayer ()
+    {
+        playerT.position = this.transform.position;
+        yield return new WaitForSeconds(0.125f);//Prevents audio distortion
+        playerScript.PlayDeathSound();
+    }
 }
