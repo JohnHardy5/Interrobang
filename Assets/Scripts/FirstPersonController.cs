9714fa6bbc6020e8af6a2c54b1f72d7b0bc14539
track394 <track394@yahoo.com>
2018-10-13 20:42:51 -0700

    Improved the jerkyness of player movement by not normalizing desired movement directions.

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -106,7 +106,8 @@ public class FirstPersonController : MonoBehaviour
         // get a normal for the surface that is being touched to move along it
         RaycastHit hitInfo;
         Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo, m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
-        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal);//.normalized;
+
         m_MoveDir.x = desiredMove.x * speed;
         m_MoveDir.z = desiredMove.z * speed;
         if (m_CharacterController.isGrounded)
