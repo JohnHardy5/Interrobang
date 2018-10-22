7ea983c9fe148841a766fcee1656c28279f72d31
track394 <track394@yahoo.com>
2018-10-22 12:26:37 -0700

    Started fixing coroutine bug

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour {
 
-    public Transform hinge;
     public Transform door;
+    public Transform hinge;
     public bool rotateClockwise;
-    public float degOfRotation;
+    public bool isOpen;
+    public int degOfRotation;
 
     private Vector3 hingePos;
-    private bool isOpen = false;
+    int rotationDir = 1;
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start()
+    {
         hingePos = hinge.position;
-	}
+        rotationDir = rotateClockwise ? 1 : -1;
+    }
 
     private void Update()
     {
@@ -23,35 +28,57 @@ public class DoorController : MonoBehaviour {
         {
             if (isOpen)
             {
-                CloseDoor();
-            } else
+                StartCoroutine(RotateDoor());
+            }
+            else
             {
-                OpenDoor();
+                StartCoroutine(RotateDoor());
             }
         }
     }
 
-    //Rotates Door about hinge into "open" position
-    public void OpenDoor()
+    IEnumerator RotateDoor()
     {
-        if (isOpen) return;
-        int rotationDir = rotateClockwise ? 1 : -1;
         for (int i = 0; i < degOfRotation; i++)
         {
-            door.RotateAround(hingePos, 1.0f * rotationDir);
+            print(Time.time);
+            yield return new WaitForSeconds(0.125f);
         }
-        isOpen = true;
     }
 
-    //Rotates Door about hinge into "closed" position
-    public void CloseDoor()
-    {
-        if (!isOpen) return;
-        int rotationDir = rotateClockwise ? 1 : -1;
-        for (int i = 0; i < degOfRotation; i++)
-        {
-            door.RotateAround(hingePos, -1.0f * rotationDir);
-        }
-        isOpen = true;
-    }
+    ////Rotates Door about hinge into "open" position
+    //public void OpenDoor()
+    //{
+    //    if (isOpen) return;
+    //    Debug.Log("Open door");
+    //    StartCoroutine("ThisIsStupid");
+    //    isOpen = true;
+    //}
+
+    ////Rotates Door about hinge into "closed" position
+    //public void CloseDoor()
+    //{
+    //    if (!isOpen) return;
+    //    Debug.Log("Close door");
+    //    StartCoroutine("ThisIsStupid");
+    //    isOpen = false;
+    //}
+
+    //IEnumerable ThisIsStupid ()
+    //{
+    //    Debug.Log("This is stupid");
+    //    yield return new WaitForSeconds(0.125f);
+    //    Debug.Log("This is stupid");
+    //}
+
+    ////IEnumerable RotateDoor()
+    ////{
+    ////    Debug.Log("rotating door");
+    ////    for (int i = 0; i < degOfRotation; i++)
+    ////    {
+    ////        door.RotateAround(hingePos, Vector3.up, 1.0f * rotationDir);
+    ////        yield return new WaitForSeconds(0.125f); // Prevents audio distortion
+    ////    }
+    ////    Debug.Log("done rotating door");
+    ////}
 }
