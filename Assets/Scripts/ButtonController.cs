da8f8053118705df226322ff6dceb5fcd750fa1f
track394 <track394@yahoo.com>
2018-10-31 19:12:10 -0700

    Started working on button auto release.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -13,15 +13,7 @@ public class ButtonController : MonoBehaviour {
 
     private bool isUp = true;
     private bool isMoving = false;
-    private bool isPressed = false;
     private int moveDir = -1;
-
-    //Figure out if we were pressed in the last frame and then release when we are no longer pressed
-    private void Update()
-    {
-        //if (!isUp) StartCoroutine(CheckButton());
-        //isPressed = false;
-    }
 
     //Move button into next state
     IEnumerator MoveButton()
@@ -36,34 +28,34 @@ public class ButtonController : MonoBehaviour {
         isMoving = false;
     }
 
-    //Check if button is depressed, wait an instant, and then check again. If the button is no longer pressed, release the button.
-    IEnumerator CheckButton()
+    //Set a timer to release the button. If the coroutine is not stopped in time, release the button.
+    IEnumerator CheckButton ()
     {
-        if (isPressed)
-        {
-            yield return new WaitForSeconds(checkTime);
-            if (!isPressed)
-            {
-                ReleaseButton();
-            }
-        }
+        yield return new WaitForSeconds(checkTime);
+        //ReleaseButton();
+        Debug.Log("Release Button");
     }
 
     //Moves button into "down" state
     public void PressButton ()
     {
-        isPressed = true;
-        if (!isUp || isMoving) return;
-        StartCoroutine(MoveButton());
-        doorToOpen.OpenDoor();
-        isUp = false;
+        StopCoroutine(CheckButton());
+        StartCoroutine(CheckButton());
+        if (isUp && !isMoving)
+        {
+            StartCoroutine(MoveButton());
+            doorToOpen.OpenDoor();
+            isUp = false;
+        }
     }
 
     //Moves button in "up" state
     public void ReleaseButton ()
     {
-        if (isUp || isMoving) return;
-        StartCoroutine(MoveButton());
-        isUp = true;
+        if (!isUp && !isMoving)
+        {
+            StartCoroutine(MoveButton());
+            isUp = true;
+        }
     }
 }
