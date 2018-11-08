2c3f13131382f75e4f9eb2c5335247f619ee8e06
track394 <track394@yahoo.com>
2018-11-08 14:31:51 -0800

    Added animation for moving platform. Working on getting player to "stick" to the platform.

---
+++ Assets/Scripts/PlayerStick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStick : MonoBehaviour {
+
+    public GameObject player;
+
+    private Quaternion rotation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            rotation = player.transform.rotation;
+            Debug.Log(rotation);
+            other.transform.parent = transform;
+            player.transform.rotation = rotation;
+            Debug.Log(player.transform.rotation);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            other.transform.parent = null;
+        }
+    }
+}
