35331405a6152948b6f1ee9e349be1c96e83a710
track394 <track394@yahoo.com>
2018-11-27 12:56:20 -0800

    Fixed player sticking glitch.

--- Assets/Scripts/PlayerStick.cs
+++ Assets/Scripts/PlayerStick.cs
@@ -6,17 +6,11 @@ public class PlayerStick : MonoBehaviour {
 
     public GameObject player;
 
-    private Quaternion playerRot;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
-            playerRot = player.transform.rotation;
-            Debug.Log(playerRot);
             other.transform.parent = transform;
-            player.transform.rotation = playerRot * transform.rotation;
-            Debug.Log(player.transform.rotation);
         }
     }
 
