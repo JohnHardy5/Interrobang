d00d228bc5447b7e1f6fe1130f884a391ea729d3
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2018-12-01 21:23:01 -0800

    Fixed looping to be smooth

--- Assets/Scripts/LoopingScript.cs
+++ Assets/Scripts/LoopingScript.cs
@@ -11,22 +11,34 @@ public class LoopingScript : MonoBehaviour {
     public GameObject SpawnPoint;
     private bool hasBeenTriggered = false;
     private Vector3 SpawnLocation;
-
+    public GameObject HallwayFloor;
+    public GameObject HallwayWall;
+    private Vector3 HallwayFloorPosition;
+    private Vector3 PlayerPosition;
+    private float playerOffsetZ;
+    private float playerOffsetY;
     // Use this for initialization
     void Start()
     {
         GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         PC = GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonController>();
         SpawnLocation = SpawnPoint.transform.position;
-        Debug.Log(PC.transform.position);
-        Debug.Log(SpawnLocation);
+        HallwayFloorPosition = HallwayFloor.transform.position;
+        PlayerPosition = PC.transform.position;
+    }
+
+    void Update()
+    {
+        playerOffsetZ = (float)(Mathf.Abs(PC.transform.position.z) - HallwayFloorPosition.z);
+        playerOffsetY = (float)(Mathf.Abs(PC.transform.position.y) - HallwayFloorPosition.y);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 SpawnLocationOffset = new Vector3(SpawnLocation.x, SpawnLocation.y + playerOffsetY, SpawnLocation.z + playerOffsetZ);
         if (!hasBeenTriggered && !isLoopingSection)
         {
-            PC.Teleport(SpawnLocation);
+            PC.Teleport(SpawnLocationOffset);
         }
         hasBeenTriggered = true;
     }
