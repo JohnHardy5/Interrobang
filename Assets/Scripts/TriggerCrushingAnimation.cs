e4c9bcc43568bf74ea673530ab5334333eeb8f16
track394 <track394@yahoo.com>
2018-12-05 14:05:52 -0800

    Started on crushing walls animation

---
+++ Assets/Scripts/TriggerCrushingAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCrushingAnimation : MonoBehaviour {
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) anim.SetTrigger("StartAnimation");
+    }
+}
