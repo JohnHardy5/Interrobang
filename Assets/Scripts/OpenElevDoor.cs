6b829c9abb3e864b882472414b80b590756c632e
joeymichaelmartinez <joeymichaelmartinez@gmail.com>
2018-12-07 02:00:39 -0800

    Made final changes to the level

---
+++ Assets/Scripts/OpenElevDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenElevDoor : MonoBehaviour {
+    public Animator anim;
+
+    private bool hasBeenTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasBeenTriggered && other.gameObject.CompareTag("Player"))
+        {
+            anim.Play("ElevatorDoorOpenAnim");
+            hasBeenTriggered = true;
+        }
+    }
+}
