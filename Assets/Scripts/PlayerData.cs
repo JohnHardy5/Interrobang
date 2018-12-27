aaabb6001ffaec6c2995e8c0deebe67a100f68da
track394 <track394@yahoo.com>
2018-12-27 11:31:52 -0800

    Added player data script to hold play time information

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿public static class PlayerData {
+    private static float playTime;
+
+    public static float PlayTime
+    {
+        get
+        {
+            return playTime;
+        }
+
+        set
+        {
+            playTime = value;
+        }
+    }
+}
